513cbef1f7834df213ab0ce540b31c028a21b66a
蔡杲沛 <gaopei.cai@ele.me>
2017-05-23 16:01:43 +0800

    add interface

--- src/main/java/eleme/openapi/sdk/api/service/UserService.java
+++ src/main/java/eleme/openapi/sdk/api/service/UserService.java
@@ -2,13 +2,12 @@ package eleme.openapi.sdk.api.service;
 
 import eleme.openapi.sdk.api.annotation.Service;
 import eleme.openapi.sdk.api.base.BaseNopService;
+import eleme.openapi.sdk.api.entity.user.OUser;
 import eleme.openapi.sdk.api.exception.ServiceException;
-import eleme.openapi.sdk.oauth.response.Token;
 import eleme.openapi.sdk.config.Config;
-import eleme.openapi.sdk.api.entity.user.*;
-import eleme.openapi.sdk.api.enumeration.user.*;
+import eleme.openapi.sdk.oauth.response.Token;
+
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,5 +28,16 @@ public class UserService extends BaseNopService {
     public OUser getUser() throws ServiceException {
         Map<String, Object> params = new HashMap<String, Object>();
         return call("eleme.user.getUser", params);
+    }
+
+    /**
+     * 获取当前授权帐号的手机号,特权接口仅部分帐号可以调用
+     *
+     * @return 手机号
+     * @throws ServiceException 服务异常
+     */
+    public String getPhoneNumber() throws ServiceException {
+        Map<String, Object> params = new HashMap<String, Object>();
+        return call("eleme.user.getPhoneNumber", params);
     }
 }
