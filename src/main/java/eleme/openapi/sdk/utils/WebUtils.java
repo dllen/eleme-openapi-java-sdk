34ceeaad33020bea75b998f2d895116b14460a2f
蔡杲沛 <gaopei.cai@ele.me>
2017-07-17 16:21:15 +0800

    add http timeout exception

--- src/main/java/eleme/openapi/sdk/utils/WebUtils.java
+++ src/main/java/eleme/openapi/sdk/utils/WebUtils.java
@@ -12,10 +12,7 @@ import eleme.openapi.sdk.oauth.response.Token;
 import javax.net.ssl.*;
 import java.io.*;
 import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
+import java.net.*;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -23,13 +20,13 @@ import java.util.*;
 import java.util.zip.GZIPInputStream;
 
 public abstract class WebUtils {
-
     private static final String DEFAULT_CHARSET = Constants.CHARSET_UTF8;
     private static final String METHOD_POST = "POST";
-    private static final String METHOD_GET = "GET";
+
     static {
         JSON.DEFFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     }
+
     private static class DefaultTrustManager implements X509TrustManager {
         public X509Certificate[] getAcceptedIssuers() {
             return null;
@@ -40,16 +37,6 @@ public abstract class WebUtils {
 
         public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
         }
-    }
-
-    public static String doPost(Config context, String url, Map<String, String> params, int connectTimeout, int readTimeout)
-            throws IOException {
-        return doPost(context, url, params, DEFAULT_CHARSET, connectTimeout, readTimeout);
-    }
-
-    public static String doPost(Config context, String url, Map<String, String> params, String charset, int connectTimeout,
-                                int readTimeout) throws IOException {
-        return doPost(context, url, params, charset, connectTimeout, readTimeout, null);
     }
 
     public static String doPost(Config context, String url,
@@ -69,14 +56,13 @@ public abstract class WebUtils {
         return _doPost(context, url, ctype, content, connectTimeout, readTimeout, headerMap);
     }
 
-    @Deprecated
     public static String doPost(Config context, String url, String ctype, byte[] content, int connectTimeout, int readTimeout)
-            throws IOException {
+            throws SocketTimeoutException, IOException {
         return _doPost(context, url, ctype, content, connectTimeout, readTimeout, null);
     }
 
     private static String _doPost(Config context, String url, String ctype, byte[] content, int connectTimeout, int readTimeout,
-                                  Map<String, String> headerMap) throws IOException {
+                                  Map<String, String> headerMap) throws SocketTimeoutException, IOException {
         HttpURLConnection conn = null;
         OutputStream out = null;
         String rsp = null;
@@ -99,55 +85,10 @@ public abstract class WebUtils {
         return rsp;
     }
 
-
-    private static byte[] getTextEntry(String fieldName, String fieldValue, String charset) throws IOException {
-        StringBuilder entry = new StringBuilder();
-        entry.append("Content-Disposition:form-data;name=\"");
-        entry.append(fieldName);
-        entry.append("\"\r\nContent-Type:text/plain\r\n\r\n");
-        entry.append(fieldValue);
-        return entry.toString().getBytes(charset);
-    }
-
-    private static byte[] getFileEntry(String fieldName, String fileName, String mimeType, String charset)
-            throws IOException {
-        StringBuilder entry = new StringBuilder();
-        entry.append("Content-Disposition:form-data;name=\"");
-        entry.append(fieldName);
-        entry.append("\";filename=\"");
-        entry.append(fileName);
-        entry.append("\"\r\nContent-Type:");
-        entry.append(mimeType);
-        entry.append("\r\n\r\n");
-        return entry.toString().getBytes(charset);
-    }
-
-    public static String doGet(String url, Map<String, String> params) throws IOException {
-        return doGet(url, params, DEFAULT_CHARSET);
-    }
-
-    public static String doGet(String url, Map<String, String> params, String charset) throws IOException {
-        HttpURLConnection conn = null;
-        String rsp = null;
-
-        try {
-            String ctype = "application/x-www-form-urlencoded;charset=" + charset;
-            String query = buildQuery(params, charset);
-            conn = getConnection(buildGetUrl(url, query), METHOD_GET, ctype, null);
-            rsp = getResponseAsString(conn);
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
-
-        return rsp;
-    }
-
     private static HttpURLConnection getConnection(URL url, String method, String ctype, Map<String, String> headerMap) throws IOException {
-        HttpURLConnection conn = null;
+        HttpURLConnection conn;
         if ("https".equals(url.getProtocol())) {
-            SSLContext ctx = null;
+            SSLContext ctx;
             try {
                 ctx = SSLContext.getInstance("TLS");
                 ctx.init(new KeyManager[0], new TrustManager[]{new DefaultTrustManager()}, new SecureRandom());
@@ -172,36 +113,13 @@ public abstract class WebUtils {
         conn.setRequestProperty("Accept", "text/xml,text/javascript,text/html");
         conn.setRequestProperty("Content-Type", ctype);
         conn.setRequestProperty("Accept-Encoding", "gzip");
-        conn.setRequestProperty("User-Agent","eleme-openapi-java-sdk");
+        conn.setRequestProperty("User-Agent", "eleme-openapi-java-sdk");
         if (headerMap != null) {
             for (Map.Entry<String, String> entry : headerMap.entrySet()) {
                 conn.setRequestProperty(entry.getKey(), entry.getValue());
             }
         }
         return conn;
-    }
-
-    private static URL buildGetUrl(String strUrl, String query) throws IOException {
-        URL url = new URL(strUrl);
-        if (StringUtils.isEmpty(query)) {
-            return url;
-        }
-
-        if (StringUtils.isEmpty(url.getQuery())) {
-            if (strUrl.endsWith("?")) {
-                strUrl = strUrl + query;
-            } else {
-                strUrl = strUrl + "?" + query;
-            }
-        } else {
-            if (strUrl.endsWith("&")) {
-                strUrl = strUrl + query;
-            } else {
-                strUrl = strUrl + "&" + query;
-            }
-        }
-
-        return new URL(strUrl);
     }
 
     public static String buildQuery(Map<String, String> params, String charset) throws IOException {
@@ -223,11 +141,9 @@ public abstract class WebUtils {
                 } else {
                     hasParam = true;
                 }
-
                 query.append(name).append("=").append(URLEncoder.encode(value, charset));
             }
         }
-
         return query.toString();
     }
 
@@ -248,7 +164,7 @@ public abstract class WebUtils {
 
     private static String getStreamAsString(InputStream stream, String charset, HttpURLConnection conn) throws IOException {
         try {
-            Reader reader = null;
+            Reader reader;
             if ("gzip".equals(conn.getContentEncoding())) {
                 reader = new InputStreamReader(new GZIPInputStream(stream), charset);
             } else {
@@ -292,59 +208,6 @@ public abstract class WebUtils {
         return charset;
     }
 
-
-    public static String decode(String value) {
-        return decode(value, DEFAULT_CHARSET);
-    }
-
-
-    public static String encode(String value) {
-        return encode(value, DEFAULT_CHARSET);
-    }
-
-
-    public static String decode(String value, String charset) {
-        String result = null;
-        if (!StringUtils.isEmpty(value)) {
-            try {
-                result = URLDecoder.decode(value, charset);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return result;
-    }
-
-
-    public static String encode(String value, String charset) {
-        String result = null;
-        if (!StringUtils.isEmpty(value)) {
-            try {
-                result = URLEncoder.encode(value, charset);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return result;
-    }
-
-
-    public static Map<String, String> splitUrlQuery(String query) {
-        Map<String, String> result = new HashMap<String, String>();
-
-        String[] pairs = query.split("&");
-        if (pairs != null && pairs.length > 0) {
-            for (String pair : pairs) {
-                String[] param = pair.split("=", 2);
-                if (param != null && param.length == 2) {
-                    result.put(param[0], param[1]);
-                }
-            }
-        }
-
-        return result;
-    }
-
     public static <T> T call(Config context, String action,
                              Map<String, Object> parameters,
                              Token token,
@@ -356,7 +219,7 @@ public abstract class WebUtils {
         String accessToken = token.getAccessToken();
         String requestId = UUID.randomUUID().toString().toLowerCase();
 
-        System.out.println("requestId: "+ requestId);
+        System.out.println("requestId: " + requestId);
         Map<String, Object> requestPayload = new HashMap<String, Object>();
         requestPayload.put("nop", "1.0.0");
         requestPayload.put("id", requestId);
@@ -373,8 +236,14 @@ public abstract class WebUtils {
         requestPayload.put("signature", signature);
 
         String requestJson = JSON.toJSONString(requestPayload, SerializerFeature.WriteDateUseDateFormat);
-        ResponsePayload responsePayload = doRequest(context, requestJson);
-
+        ResponsePayload responsePayload = null;
+        try {
+            responsePayload = doRequest(context, requestJson);
+        } catch (SocketTimeoutException ex) {
+            throw new SourceTimeoutException();
+        } catch (IOException ex) {
+            throw new ServiceException(ex.getClass().getName(), ex);
+        }
         setLogInfo(context, "request: " + requestJson);
         if (responsePayload != null && null != responsePayload.getError()) {
             ServiceException serviceException = toException(responsePayload.getError());
@@ -386,19 +255,14 @@ public abstract class WebUtils {
         }
         if (type == void.class)
             return null;
-        String s2 = JSON.toJSONString(responsePayload.getResult(),SerializerFeature.WriteDateUseDateFormat);
+        String s2 = JSON.toJSONString(responsePayload.getResult(), SerializerFeature.WriteDateUseDateFormat);
         return JSON.parseObject(s2, type);
     }
 
-    private static ResponsePayload doRequest(Config context, String requestJson) {
-        try {
-            String response = doPost(context, context.getApiUrl(), "application/json; charset=utf-8", requestJson.getBytes(Constants.CHARSET_UTF8), 15000, 30000);
-            setLogInfo(context, "response: " + response);
-            return JSON.parseObject(response, ResponsePayload.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    private static ResponsePayload doRequest(Config context, String requestJson) throws SocketTimeoutException, IOException {
+        String response = doPost(context, context.getApiUrl(), "application/json; charset=utf-8", requestJson.getBytes(Constants.CHARSET_UTF8), 15000, 15000);
+        setLogInfo(context, "response: " + response);
+        return JSON.parseObject(response, ResponsePayload.class);
     }
 
     private static ServiceException toException(ErrorPayload error) throws ServiceException {
